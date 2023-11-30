f3c75a6987155631431313d798c392c18a40fca0
MatteoGiuntella <matteo.giuntella@virgilio.it>
2023-11-30 17:03:24 +0100

    fine

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+// Chiedi all'utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito114
+
+
+const nome = prompt ('Come ti chiami?') ;
+
+const cognome = prompt ('quale è il tuo cognome?') ;
+
+const colore = prompt ('e il colore preferito?') ;
+
+const password = nome + cognome + colore + '114' ;
+
+const myElement = document.getElementById('text-target') ;
+myElement.innerHTML = `La tua password è: ${password}`;
+
+
+
+/*
+let domanda = prompt ("nome?")
+
+let password2 = domanda;
+
+domanda = prompt("cognome?")
+
+password2 = password2 + domanda;
+
+domanda = prompt("colore?")
+
+password2 = password2 + domanda + "114";
+
+const myElement = document.getElementById('text-target') ;
+myElement.innerHTML = `La tua password è: ${password2}`;
+*/
